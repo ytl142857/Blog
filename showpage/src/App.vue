6e8227fa4a7c9b0ac97a3bd19829e131588502b9
ytl142857 <2854669890@qq.com>
2022-03-01 15:20:10 +0800

    showpage init

---
+++ showpage/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-app>
+    <Navbar />
+    <v-main>
+      <router-view />
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import Navbar from "./components/Navbar.vue";
+
+export default {
+  name: "App",
+  components: {
+    Navbar,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ showpage/src/components/Navbar.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-app-bar app color="primary ">
+    <div class="d-flex align-center">
+      <v-img
+        alt="Vuetify Logo"
+        class="shrink mr-2"
+        contain
+        src="../assets/logo.jpg"
+        transition="scale-transition"
+        width="40"
+      />
+
+    </div>
+
+    <v-spacer></v-spacer>
+  </v-app-bar>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+</style>
