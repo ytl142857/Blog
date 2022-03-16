45fd3dc0e0abdd7291c0f494de9da0b984e73e25
ytl142857 <2854669890@qq.com>
2022-03-16 14:14:42 +0800

    add gov info

--- showpage/src/components/Footer.vue
+++ showpage/src/components/Footer.vue
@@ -2,24 +2,25 @@
   <v-footer absolute padless>
     <v-card class="text-center" width="100%" flat tile color="white">
       <v-divider></v-divider>
-      <!-- <v-card-text>
-        icon here
-      </v-card-text> -->
-      
+
       <v-card-text class="myPrimary--text font-weight-light">
-        {{ new Date().getFullYear() }} Chestnutttt | 
-        <a target="_blank" href="https://beian.miit.gov.cn/">渝ICP备2022002492号-1</a>
+        {{ new Date().getFullYear() }} Chestnutttt |
+        <a target="_blank" href="https://beian.miit.gov.cn/"
+          >渝ICP备2022002492号-1</a
+        >
+        | <a
+          target="_blank"
+          href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=50011202503295"
+          ><v-avatar size="16px"><img src="@/assets/gov.png" /></v-avatar> 渝公网安备 50011202503295号</a
+        >
       </v-card-text>
     </v-card>
   </v-footer>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style lang="scss" scoped>
-
 </style>
